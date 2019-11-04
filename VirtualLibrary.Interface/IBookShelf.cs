e96983779cfc2241b88c5c66f627309ee06bfbe7
Devon Tinsley <dtinsley@peerlessproducts.com>
2019-11-04 12:38:57 -0600

    added basic bookshelf interface

---
+++ VirtualLibrary.Interface/IBookShelf.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VirtualLibrary.Interface
+{
+    public interface IBookShelf
+    {
+
+
+        //C.R.U.D.
+
+        IEnumerable<Book> GetBooks(); //r
+        Book GetBook(int id); //r
+        void AddBook(Book newBook); //c
+        void UpdateBook(int id, Book updatedBook); //u
+        void DeleteBook(int id); //d
+
+
+
+
+
+    }
+}
---
+++ VirtualLibrary.Service/TechBookShelf.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using VirtualLibrary.Interface;
+
+namespace VirtualLibrary.Service
+{
+    public class TechBookShelf : IBookShelf
+    {
+        public void AddBook(Book newBook)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteBook(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book GetBook(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Book> GetBooks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateBook(int id, Book updatedBook)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
